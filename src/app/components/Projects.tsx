cb5e3300a1f3e9a4c6811bf6d3e9c31cfb7df1ad
xyugen <alex.arias.jorym@gmail.com>
2024-08-30 02:57:20 +0800

    refactor: format projects into an array

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -10,99 +10,94 @@ import {
     SquareTerminal,
 } from "lucide-react";
 
+const projects = [
+    {
+        title: "Anonymyst",
+        url: "https://github.com/xyugen/anonymyst",
+        icon: <ScrollText className="size-4" />,
+        description:
+            "A digital platform that allows users to express themselves freely and anonymously, similar to a classic freedom wall.",
+        tags: ["React"],
+    },
+    {
+        title: "Gitignite",
+        url: "https://github.com/xyugen/gitignite",
+        icon: <SquareTerminal className="size-4" />,
+        description: (
+            <>
+                A command-line tool for generating <i>.gitignore</i> files from
+                language templates provided by <code>github/gitignore</code>.
+            </>
+        ),
+        tags: ["Golang"],
+    },
+    {
+        title: "BulSU Schedule Plotter",
+        icon: <CalendarDays className="size-4" />,
+        description: (
+            <>
+                A web application created for <B>Bulacan State University</B> to
+                help instructors plot and visualize their schedules.
+            </>
+        ),
+        tags: ["React", "NextJS"],
+    },
+    {
+        title: "Realtime Chat",
+        url: "https://github.com/xyugen/realtime-chat-frontend",
+        icon: <MessageCircleMore className="size-4" />,
+        description: (
+            <>A realtime chat web application for learning WebSockets.</>
+        ),
+        tags: ["Solid", "Golang"],
+    },
+    {
+        title: "Revel Bot",
+        url: "https://github.com/xyugen/revel-bot",
+        icon: <MessageCircleMore className="size-4" />,
+        description: (
+            <>
+                A Discord bot created for my personal server integrated with{" "}
+                <B>Artificial Intelligence</B> and other utility tools.
+            </>
+        ),
+        tags: ["Discord.js", "JavaScript"],
+    },
+    {
+        title: "Pixiframe",
+        url: "https://github.com/xyugen/pixiframe",
+        icon: <ImageDown className="size-4" />,
+        description: (
+            <>
+                A stunning website that empowers you to upload, share, and
+                secure images with ease.
+            </>
+        ),
+        tags: ["SvelteKit", "Supabase"],
+    },
+    {
+        title: "Trade Document Request",
+        url: "https://github.com/xyugen/trade-docrqst",
+        icon: <FileInput className="size-4" />,
+        description: (
+            <>
+                An android application for requesting documents on{" "}
+                <B>San Jose Del Monte National Trade School</B>.
+            </>
+        ),
+        tags: ["Android Studio", "Java"],
+    },
+];
+
 const Projects = () => {
     return (
         <section id="projects" className="space-y-4 ">
             <H2>Projects</H2>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-                <Project
-                    title="Anonymyst"
-                    url="https://github.com/xyugen/anonymyst"
-                    icon={<ScrollText className="size-4" />}
-                    description="A digital platform that allows users to express themselves freely and anonymously, similar to a classic freedom wall. "
-                    tags={["React"]}
-                />
-
-                <Project
-                    title="Gitignite"
-                    url="https://github.com/xyugen/gitignite"
-                    icon={<SquareTerminal className="size-4" />}
-                    description={
-                        <>
-                            A command-line tool for generating <i>.gitignore</i>{" "}
-                            files from language templates provided by{" "}
-                            <code>github/gitignore</code>.
-                        </>
-                    }
-                    tags={["Golang"]}
-                />
-
-                <Project
-                    title="BulSU Schedule Plotter"
-                    icon={<CalendarDays className="size-4" />}
-                    description={
-                        <>
-                            A web application created for{" "}
-                            <B>Bulacan State University</B> to help instructors
-                            plot and visualize their schedules.
-                        </>
-                    }
-                    tags={["React", "NextJS"]}
-                />
-
-                <Project
-                    title="Realtime Chat"
-                    url="https://github.com/xyugen/realtime-chat-frontend"
-                    icon={<MessageCircleMore className="size-4" />}
-                    description={
-                        <>
-                            A realtime chat web application for learning
-                            WebSockets.
-                        </>
-                    }
-                    tags={["Solid", "Golang"]}
-                />
-
-                <Project
-                    title="Revel Bot"
-                    url="https://github.com/xyugen/revel-bot"
-                    icon={<MessageCircleMore className="size-4" />}
-                    description={
-                        <>
-                            A Discord bot created for my personal server
-                            integrated with <B>Artificial Intelligence</B> and
-                            other utility tools.
-                        </>
-                    }
-                    tags={["Discord.js", "JavaScript"]}
-                />
-
-                <Project
-                    title="Pixiframe"
-                    url="https://github.com/xyugen/pixiframe"
-                    icon={<ImageDown className="size-4" />}
-                    description={
-                        <>
-                            A stunning website that empowers you to upload,
-                            share, and secure images with ease.
-                        </>
-                    }
-                    tags={["SvelteKit", "Supabase"]}
-                />
-
-                <Project
-                    title="Trade Document Request"
-                    url="https://github.com/xyugen/trade-docrqst"
-                    icon={<FileInput className="size-4" />}
-                    description={
-                        <>
-                            An android application for requesting documents on{" "}
-                            <B>San Jose Del Monte National Trade School</B>.
-                        </>
-                    }
-                    tags={["Android Studio", "Java"]}
-                />
+                {projects.map((project, index) => (
+                    <Project key={index} {...project} />
+                ))}
             </div>
         </section>
     );
