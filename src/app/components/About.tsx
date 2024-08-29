d2ce84c7cf9ab53b3008087d6fd37833a53dc109
xyugen <alex.arias.jorym@gmail.com>
2024-08-30 01:23:14 +0800

    feat: change about description

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
         <section id="about" className="space-y-4">
             <H2>Renzyx</H2>
             <p className="text-foreground text-base">
-                I&rsquo;m Renzyx, a passionate web developer with a keen eye for
+                Hello, I am web developer with a keen eye for
                 design and a love for clean, efficient code. I specialize in
                 creating responsive and user-friendly web applications using
                 modern technologies.
