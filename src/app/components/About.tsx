1a977801235fbf08735b621935e7e2643c1fe25e
xyugen <alex.arias.jorym@gmail.com>
2024-08-30 01:42:01 +0800

    feat: add github logo and link

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,14 +1,22 @@
 import H2 from "@/components/H2";
+import Github from "@/components/icons/Github";
 
 const About = () => {
     return (
         <section id="about" className="space-y-4">
-            <H2>Renzyx</H2>
+            <div className="flex flex-row items-center gap-2">
+                <H2>Renzyx</H2>
+                <a
+                    href="https://github.com/xyugen"
+                    className="transition-colors cursor-pointer"
+                >
+                    <Github className="w-4 h-4 text-muted-foreground hover:text-primary" />
+                </a>
+            </div>
             <p className="text-foreground text-base">
-                Hello, I am web developer with a keen eye for
-                design and a love for clean, efficient code. I specialize in
-                creating responsive and user-friendly web applications using
-                modern technologies.
+                Hello, I am web developer with a keen eye for design and a love
+                for clean, efficient code. I specialize in creating responsive
+                and user-friendly web applications using modern technologies.
             </p>
         </section>
     );
--- src/components/H2.tsx
+++ src/components/H2.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const H2 = ({ children }: { children: React.ReactNode }) => {
-    return <h2 className="text-primary text-xl font-bold">{children}</h2>;
+    return <h2 className="text-primary text-lg font-bold">{children}</h2>;
 };
 
 export default H2;
