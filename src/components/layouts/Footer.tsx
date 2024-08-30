8507cc50f5a347654f681cc49b56196fbf578df3
xyugen <alex.arias.jorym@gmail.com>
2024-08-30 16:40:55 +0800

    feat: add favicon and padding to larger screens on layouts

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -2,7 +2,7 @@ import ThemeToggle from "../theme-toggle";
 
 const Footer = () => {
     return (
-        <footer className="bg-background border-t border-t-muted p-8 z-50">
+        <footer className="bg-background border-t border-t-muted p-8 xl:px-20 z-50">
             <div className="flex flex-col md:flex-row items-start md:items-end justify-between">
                 <p className="font-extrabold text-5xl md:text-9xl bg-gradient-to-r from-slate-950 to-slate-500 dark:from-slate-500 dark:to-slate-100 bg-clip-text text-transparent">
                     Renzyx.
